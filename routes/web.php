dac0c7826cf5bd86c939f96a9065b7812817c146
siwarhajhassen <siwarhajhassen7@gmail.com>
2025-08-19 17:41:21 +0100

    fix middleware

--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+}); 
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-});
-// ->middleware(['auth', 'verified'])->name('dashboard')
+})->middleware(['auth', 'verified'])->name('dashboard');
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
